9b442a67a9fd4d65b8a904d1c91e6ecebae2d2a6
danielfdezweb <danielfdezweb@gmail.com>
2025-05-27 15:51:26 +0200

    Update GetApplicablePriceUseCaseTest.java

--- prices/src/test/java/com/danielfdez/prices/GetApplicablePriceUseCaseTest.java
+++ prices/src/test/java/com/danielfdez/prices/GetApplicablePriceUseCaseTest.java
@@ -9,12 +9,10 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Collections;
-
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
