b2e67a9c49dd927d88f7896a988a7167a0202e31
danielfdezweb <danielfdezweb@gmail.com>
2025-05-27 15:49:39 +0200

    Delete prices directory

--- prices/src/main/java/com/danielfdez/prices/domain/repository/PriceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.danielfdez.prices.domain.repository;
-
-import com.danielfdez.prices.domain.model.Price;
-import java.time.LocalDateTime;
-import java.util.List;
-
-public interface PriceRepository {
-    List<Price> findByProductIdAndBrandIdAndDate(Long productId, Long brandId, LocalDateTime date);
-}
